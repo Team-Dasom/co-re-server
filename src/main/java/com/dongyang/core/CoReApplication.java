1866b6039e7fb5fd633141e0ccc4649e2233b7af
JUN <chltmdwns96@naver.com>
2023-05-16 14:26:56 +0900

    project init

---
+++ src/main/java/com/dongyang/core/CoReApplication.java
@@ -0,0 +1,13 @@
+package com.dongyang.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoReApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoReApplication.class, args);
+	}
+
+}
