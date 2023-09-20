6c85a9ac6228ae65ac3d55b9898552375290cdcc
PgmJun <chltmdwns96@gmail.com>
2023-09-21 02:52:13 +0900

    refactor: 설명 주석 기능 요청 최대한 간략하게 수정

--- src/main/java/com/dongyang/core/external/gpt/dto/gpt/GptRequest.java
+++ src/main/java/com/dongyang/core/external/gpt/dto/gpt/GptRequest.java
@@ -16,6 +16,6 @@ public class GptRequest {
 	}
 
 	public void formatAddCommentRequest() {
-		content = content + "\n이 코드는 "+language+"로 작성한 코드이다. 어떤 기능을 하는 지에 대해 한글로 설명 주석을 달아서 코드만 보여줘";
+		content = content + "\n이 코드는 "+language+"로 작성한 코드이다. 어떤 기능을 하는 지에 대해 한글로 설명 주석을 달아서 코드만 최대한 간략하게 보여줘";
 	}
 }
