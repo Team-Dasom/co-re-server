feb4fab0aede728e0255a300bc0985437fe02461
PgmJun <chltmdwns96@gmail.com>
2023-10-14 01:32:30 +0900

    ♻️: refactor GPT 관련 설정값 enum으로 관리 #8

---
+++ src/main/java/com/dongyang/core/domain/gpt/GptFunction.java
@@ -0,0 +1,17 @@
+package com.dongyang.core.domain.gpt;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public enum GptFunction {
+	RECOMMEND_VARIABLE_NAME("당신은 %s 변수명 추천 전문가이다. 단어를 입력받으면 그 단어와 정말 어울리는 변수명을 10개 추천해줘", 0.7, 2000),
+	ADD_COMMENT("당신은 코드와 코드를 작성한 프로그래밍 언어를 입력받으면 코드를 분석한 후 주석을 달아주는 어시스트이다. 값을 입력받으면 주석으로 코드가 어떤 동작을 하는지에 대해서 '주석\\n코드' 형태로 간략한 설명을 달아서 리턴해줘", 0.2, 100),
+	;
+
+	private final String systemRoleMessage;
+	private final double temperature;
+	private final int maxToken;
+}
