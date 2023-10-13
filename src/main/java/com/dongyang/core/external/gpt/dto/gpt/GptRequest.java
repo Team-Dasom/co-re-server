d908cf5aafdcc4409813e75f531abfbf5e5411bd
PgmJun <chltmdwns96@gmail.com>
2023-10-14 01:33:41 +0900

    ♻️: refactor Gpt 기능요청 DTO에 function 필드 추가 #8

--- src/main/java/com/dongyang/core/external/gpt/dto/gpt/GptRequest.java
+++ src/main/java/com/dongyang/core/external/gpt/dto/gpt/GptRequest.java
@@ -1,21 +1,19 @@
 package com.dongyang.core.external.gpt.dto.gpt;
 
+import com.dongyang.core.domain.gpt.GptFunction;
+
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
-@NoArgsConstructor
-@AllArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Getter
 public class GptRequest {
+	private GptFunction function;
 	private String content;
 	private String language;
 
-	public void formatRequestRecommendVariableQuestion() {
-		content = "'" + content + "' 라는 단어에 대해 " + language + "언어 기준으로 변수명 10개 추천해줘.";
-	}
-
-	public void formatAddCommentRequest() {
-		content = content + "\n이 코드는 "+language+" 언어로 작성한 코드이다. 코드의 기능 대해 각 코드의 윗 줄에 간단히 설명 주석을 달아서 코드 블럭형태로 리턴해줘";
+	public String formatAddCommentRequest() {
+		return language + " 언어로 개발된 코드\n" + content;
 	}
 }
