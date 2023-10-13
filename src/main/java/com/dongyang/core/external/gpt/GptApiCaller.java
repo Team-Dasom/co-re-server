88bac3a7d1c9b1aaf93ca8bc186709d01441d818
PgmJun <chltmdwns96@gmail.com>
2023-10-14 01:35:34 +0900

    ♻️: refactor Service 레이어에서 Gpt 기능요청 Message 생성하여 제공하도록 수정 #8

--- src/main/java/com/dongyang/core/external/gpt/GptApiCaller.java
+++ src/main/java/com/dongyang/core/external/gpt/GptApiCaller.java
@@ -1,9 +1,12 @@
 package com.dongyang.core.external.gpt;
 
+import java.util.List;
+
 import com.dongyang.core.external.gpt.dto.gpt.GptQuestionResponseDto;
 import com.dongyang.core.external.gpt.dto.gpt.GptRequest;
+import com.dongyang.core.external.gpt.dto.gpt.GptMessage;
 
 public interface GptApiCaller {
 
-	GptQuestionResponseDto sendRequest(GptRequest request, int maxTokenValue);
+	GptQuestionResponseDto sendRequest(GptRequest request, List<GptMessage> requestBody);
 }
