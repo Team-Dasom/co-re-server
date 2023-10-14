5fda4daa27c05c264d22e732106666c5a8d68ee9
PgmJun <chltmdwns96@gmail.com>
2023-10-14 14:38:10 +0900

    ⚡️: fix '주석 설명' 기능 시스템 메세지 변경하여 응답 정확도 상승 #8

--- src/main/java/com/dongyang/core/domain/gpt/constant/GptFunction.java
+++ src/main/java/com/dongyang/core/domain/gpt/constant/GptFunction.java
@@ -8,7 +8,7 @@ import lombok.Getter;
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public enum GptFunction {
 	RECOMMEND_VARIABLE_NAME("당신은 %s 변수명 추천 전문가이다. 단어를 입력받으면 그 단어와 정말 어울리는 변수명을 10개 추천해줘", 0.7, 100),
-	ADD_COMMENT("당신은 프로그래밍 언어와 그 언어로 작성된 코드를 받으면 이를 분석하는 코드 전문가이다. 값을 입력받으면 각각의 코드가 어떤 동작을 하는지 간략하게 설명한 주석을 {주석}\\n{코드}' 형태로 달아서 리턴해줘", 0.2, 2000),
+	ADD_COMMENT("당신은 프로그래밍 언어와 그 언어로 작성된 코드를 받으면 이를 분석하는 코드 전문가이다. 값을 입력받으면 각각의 코드가 어떤 동작을 하는지 간략하게 설명한 주석을 각 코드 라인 별로 {주석}\\n{코드}' 형태로 달아서 리턴해줘", 0.1, 2000),
 	;
 
 	private final String systemRoleMessage;
