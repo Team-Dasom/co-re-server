11c0138adccea2cb08f9494a45b280c6d16301b3
PgmJun <chltmdwns96@gmail.com>
2023-10-20 21:46:26 +0900

    ♻️: refactor GPT Model명 상수로 관리 #27

--- src/main/java/com/dongyang/core/domain/gpt/constant/GptConstant.java
+++ src/main/java/com/dongyang/core/domain/gpt/constant/GptConstant.java
@@ -4,7 +4,8 @@ public class GptConstant {
 
 	public final static String MESSAGE_SYSTEM = "system";
 	public final static String MESSAGE_USER = "user";
-	public final static String GPT_MODEL_NAME = "gpt-3.5-turbo";
+	public final static String GPT_DEFAULT_MODEL = "gpt-3.5-turbo";
+	public final static String GPT_RECOMMEND_VARIABLE_NAME_MODEL = "ft:gpt-3.5-turbo-0613:personal::8BfCxWMa";
 	public final static String MODEL = "model";
 	public final static String MAX_TOKENS = "max_tokens";
 	public final static String MESSAGES = "messages";
