5f007663b98a944e5782c8185a385e8fa7393fd7
PgmJun <chltmdwns96@gmail.com>
2023-10-20 22:25:25 +0900

    ✨: feat 설명 주석 추가 기능 Fine-Tuning #27

--- src/main/java/com/dongyang/core/domain/gpt/constant/GptConstant.java
+++ src/main/java/com/dongyang/core/domain/gpt/constant/GptConstant.java
@@ -6,6 +6,7 @@ public class GptConstant {
 	public final static String MESSAGE_USER = "user";
 	public final static String GPT_DEFAULT_MODEL = "gpt-3.5-turbo";
 	public final static String GPT_RECOMMEND_VARIABLE_NAME_MODEL = "ft:gpt-3.5-turbo-0613:personal::8BfCxWMa";
+	public final static String GPT_ADD_COMMENT_MODEL = "ft:gpt-3.5-turbo-0613:personal::8BkFCloU";
 	public final static String MODEL = "model";
 	public final static String MAX_TOKENS = "max_tokens";
 	public final static String MESSAGES = "messages";
