be5f82f30e8c5fd59b0a746d38871245f5b5bd39
PgmJun <chltmdwns96@gmail.com>
2023-10-23 02:32:10 +0900

    ♻️: refactor GPT Model명 환경변수로 추가하여 Config에서 관리하도록 수정 #27

--- src/main/java/com/dongyang/core/domain/gpt/constant/GptConstant.java
+++ src/main/java/com/dongyang/core/domain/gpt/constant/GptConstant.java
@@ -1,17 +1,11 @@
 package com.dongyang.core.domain.gpt.constant;
 
 public class GptConstant {
-
-
-	public final static String MESSAGE_SYSTEM = "system";
-	public final static String MESSAGE_USER = "user";
-	public final static String GPT_DEFAULT_MODEL = "gpt-3.5-turbo";
-	public final static String GPT_RECOMMEND_VARIABLE_NAME_MODEL = "ft:gpt-3.5-turbo-0613:personal::8BfCxWMa";
-	public final static String GPT_ADD_COMMENT_MODEL = "ft:gpt-3.5-turbo-0613:personal::8BkFCloU";
-	public final static String GPT_REFACTOR_CODE_MODEL = "ft:gpt-3.5-turbo-0613:personal::8C8c5OLe";
-	public final static String MODEL = "model";
-	public final static String MAX_TOKENS = "max_tokens";
-	public final static String MESSAGES = "messages";
-	public final static String TEMPERATURE = "temperature";
-	public final static String GPT_REQUEST_VALUE_ERROR_SIGN = "X";
+    public final static String MESSAGE_SYSTEM = "system";
+    public final static String MESSAGE_USER = "user";
+    public final static String MODEL = "model";
+    public final static String MAX_TOKENS = "max_tokens";
+    public final static String MESSAGES = "messages";
+    public final static String TEMPERATURE = "temperature";
+    public final static String GPT_REQUEST_VALUE_ERROR_SIGN = "X";
 }
