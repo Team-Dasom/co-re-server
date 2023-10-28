c5e58a10c9909c0d1c11d9626544ab8a5fbe8f80
PgmJun <chltmdwns96@gmail.com>
2023-10-28 15:30:23 +0900

    ⚡️: fix WebClient 타임아웃 시간 변경 #33
    20000 -> 30000

--- src/main/java/com/dongyang/core/external/config/WebClientConfig.java
+++ src/main/java/com/dongyang/core/external/config/WebClientConfig.java
@@ -27,9 +27,9 @@ public class WebClientConfig {
 			.clientConnector(new ReactorClientHttpConnector(
 				HttpClient.from(
 					TcpClient.create()
-						.option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 20000)
+						.option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 30000)
 						.doOnConnected(conn ->
-							conn.addHandler(new ReadTimeoutHandler(20000, TimeUnit.MILLISECONDS))
+							conn.addHandler(new ReadTimeoutHandler(30000, TimeUnit.MILLISECONDS))
 						)
 				)
 			))
