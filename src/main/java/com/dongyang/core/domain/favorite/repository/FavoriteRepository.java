ec5e650330c494b646577a5502a85d089ccb65f6
PgmJun <chltmdwns96@gmail.com>
2023-11-11 00:13:13 +0900

    ♻️: refactor 코드블럭 간격 조정 #37

--- src/main/java/com/dongyang/core/domain/favorite/repository/FavoriteRepository.java
+++ src/main/java/com/dongyang/core/domain/favorite/repository/FavoriteRepository.java
@@ -3,6 +3,6 @@ package com.dongyang.core.domain.favorite.repository;
 import com.dongyang.core.domain.favorite.Favorite;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface FavoriteRepository extends JpaRepository<Favorite, Long>, FavoriteRepositoryCustom{
+public interface FavoriteRepository extends JpaRepository<Favorite, Long>, FavoriteRepositoryCustom {
 
 }
