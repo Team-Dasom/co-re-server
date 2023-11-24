f6439dae52fbc01983d14ec4290780ef3084e464
PgmJun <chltmdwns96@gmail.com>
2023-11-24 19:39:01 +0900

    ⚡️: fix 1분에 10개 토큰 사용 가능하도록 설정 #25

--- src/main/java/com/dongyang/core/global/common/filter/rateLimit/BucketPlan.java
+++ src/main/java/com/dongyang/core/global/common/filter/rateLimit/BucketPlan.java
@@ -14,7 +14,7 @@ public enum BucketPlan {
     //1시간에 3번 사용가능한 무제한 요금제
     MEMBER {
         public Bandwidth getLimit() {
-            return Bandwidth.classic(1, Refill.intervally(1, Duration.ofMinutes(1)));
+            return Bandwidth.classic(10, Refill.intervally(10, Duration.ofMinutes(1)));
         }
     };
 
