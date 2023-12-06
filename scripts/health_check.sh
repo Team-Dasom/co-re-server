8cd1638040bd5bb2e6ccb9719343d05f821c8049
PgmJun <chltmdwns96@gmail.com>
2023-12-06 23:49:21 +0900

    🐛: bugfix 로깅 시에 Permission Denied로 인한 문제 해결 #49

--- scripts/health_check.sh
+++ scripts/health_check.sh
@@ -9,22 +9,22 @@ if [ ${CURRENT_PORT} -eq 8081 ]; then
 elif [ ${CURRENT_PORT} -eq 8082 ]; then
   TARGET_PORT=8081
 else
-  echo "[$NOW_TIME] No WAS is connected to nginx" >> /home/ubuntu/api-server/deploy_err.log
+  echo "[$NOW_TIME] No WAS is connected to nginx"
   exit 1
 fi
 
-echo "[$NOW_TIME] Start health check of WAS at 'http://127.0.0.1:${TARGET_PORT}' ..." >> /home/ubuntu/api-server/deploy.log
+echo "[$NOW_TIME] Start health check of WAS at 'http://127.0.0.1:${TARGET_PORT}' ..."
 
 for RETRY_COUNT in 1 2 3 4 5 6 7 8 9 10
 do
-  echo "[$NOW_TIME] #${RETRY_COUNT} trying..." >> /home/ubuntu/api-server/deploy.log
+  echo "[$NOW_TIME] #${RETRY_COUNT} trying..."
   RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://127.0.0.1:${TARGET_PORT}/actuator/health)
 
   if [ ${RESPONSE_CODE} -eq 200 ]; then
-    echo "[$NOW_TIME] New WAS successfully running" >> /home/ubuntu/api-server/deploy.log
+    echo "[$NOW_TIME] New WAS successfully running"
     exit 0
   elif [ ${RETRY_COUNT} -eq 10 ]; then
-    echo "[$NOW_TIME] Health check failed." >> /home/ubuntu/api-server/deploy_err.log
+    echo "[$NOW_TIME] Health check failed."
     exit 1
   fi
   sleep 10
