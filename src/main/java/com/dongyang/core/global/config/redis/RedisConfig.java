3f828354e055a434ae52526df3f490805c72b080
PgmJun <chltmdwns96@gmail.com>
2023-12-27 21:05:08 +0900

    ✨: feat Redis Transaction 설정 적용 #53

--- src/main/java/com/dongyang/core/global/config/redis/RedisConfig.java
+++ src/main/java/com/dongyang/core/global/config/redis/RedisConfig.java
@@ -1,5 +1,7 @@
 package com.dongyang.core.global.config.redis;
 
+import javax.sql.DataSource;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,16 +10,19 @@ import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactor
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.session.data.redis.config.ConfigureRedisAction;
-
-import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @RequiredArgsConstructor
 @Configuration
 @EnableRedisRepositories
+@EnableTransactionManagement
 public class RedisConfig {
 
 	private final RedisProperties redisProperties;
+	private final DataSource dataSource;
 
 	@Bean
 	public RedisConnectionFactory redisConnectionFactory() {
@@ -30,7 +35,14 @@ public class RedisConfig {
 		redisTemplate.setConnectionFactory(redisConnectionFactory());
 		redisTemplate.setKeySerializer(new StringRedisSerializer());
 		redisTemplate.setValueSerializer(new StringRedisSerializer());
+		redisTemplate.setEnableTransactionSupport(true); // Redis Transaction ON
 		return redisTemplate;
+	}
+
+	// 트랜잭션 적용을 위한 TransactionManager Bean 등록
+	@Bean
+	public PlatformTransactionManager transactionManager() {
+		return new DataSourceTransactionManager(dataSource);
 	}
 
 	@Bean
